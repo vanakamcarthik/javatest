297329b15eb5f5acb9c02b1b5ac05845cde257c9
vanakamcarthik <154732944+vanakamcarthik@users.noreply.github.com>
2024-02-24 15:50:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,17 @@
-/** 
- * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
- */
+import java.util.Scanner;
+
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-  }
-}
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+    }
